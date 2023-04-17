14e33e76360ed64b875c01c823987b00dc6eb978
Taize Moreira <taizebueno@uniplaclages.edu.br>
2023-04-17 14:37:50 -0300

    "Nós nunca desistimos, nós apenas atualizamos o código"

---
+++ CadastroDeEquipamentos.ConsoleApp/ModuloChamados/Chamado.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CadastroDeEquipamentos.ConsoleApp.ModuloChamados
+{
+    internal class Chamado
+    {
+    }
+}
